1597eec3017debf293f195d75b9232916efa9369
n1xon <nikschankin@gmail.com>
2026-04-16 15:44:41 +0300

    Add configuration files for parser and bookshop in likec4

---
+++ bookshop/likec4.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'likec4/config'
+
+export default defineConfig({
+  name: 'bookshop-likec4'
+})
---
+++ parser/likec4.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'likec4/config'
+
+export default defineConfig({
+  name: 'parser-likec4'
+})
